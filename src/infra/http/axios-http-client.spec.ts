0631939f854280e0654f9f3341e30c9bf800e147
gbelther <belthergabriel@gmail.com>
2022-06-30 22:08:01 -0300

    refactor: return mockedAxios at the makeSut

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -26,27 +26,28 @@ const mockAxios = () => {
 
 type SutTypes = {
   sut: AxiosHttpClient;
+  mockedAxios: jest.Mocked<typeof axios>;
 };
 
 const makeSut = (): SutTypes => {
   const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
   return {
     sut,
+    mockedAxios,
   };
 };
 
 describe('AxiosHttpClient', () => {
   it('should call axios with correct values', async () => {
-    const mockedAxios = mockAxios();
-    const { sut } = makeSut();
+    const { sut, mockedAxios } = makeSut();
     const requestParams = makeHttpRequest();
     await sut.request(requestParams);
     expect(mockedAxios.request).toHaveBeenCalledWith(requestParams);
   });
 
   it('should return the correct value', async () => {
-    const mockedAxios = mockAxios();
-    const { sut } = makeSut();
+    const { sut, mockedAxios } = makeSut();
     const httpResponse = await sut.request(makeHttpRequest());
     const axiosResponse = await mockedAxios.request.mock.results[0].value;
     expect(httpResponse).toEqual({
@@ -56,9 +57,8 @@ describe('AxiosHttpClient', () => {
   });
 
   it('should throw with correct error', async () => {
-    const mockedAxios = mockAxios();
+    const { sut, mockedAxios } = makeSut();
     mockedAxios.request.mockRejectedValueOnce({ response: makeHttpResponse() });
-    const { sut } = makeSut();
     const httpResponsePromise = sut.request(makeHttpRequest());
     const axiosResponse = mockedAxios.request.mock.results[0].value;
     expect(httpResponsePromise).toEqual(axiosResponse);
