ab9d778804672f651c9af44b1a317a6eabdaf552
gbelther <belthergabriel@gmail.com>
2022-06-30 22:06:08 -0300

    feat: ensure AxiosHttpClient throw with correct value

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -16,13 +16,20 @@ interface HttpClient {
 
 export class AxiosHttpClient implements HttpClient {
   async request(data: HttpRequest): Promise<HttpResponse> {
-    const axiosResponse = await axios.request({
-      url: data.url,
-      method: data.method,
-    });
-    return {
-      data: axiosResponse.data,
-      status: axiosResponse.status,
-    };
+    try {
+      const axiosResponse = await axios.request({
+        url: data.url,
+        method: data.method,
+      });
+      return {
+        data: axiosResponse.data,
+        status: axiosResponse.status,
+      };
+    } catch (error) {
+      return {
+        data: error.response.data,
+        status: error.response.status,
+      };
+    }
   }
 }
