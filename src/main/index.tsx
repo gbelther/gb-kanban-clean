db337a769babac7ee06dc934a5bd232adae8ce6f
gbelther <belthergabriel@gmail.com>
2022-07-03 19:48:27 -0300

    feat: config react and react-dom

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
