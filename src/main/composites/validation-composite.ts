42afe44483144e2bcde3005408f7dcd54d6c7062
gbelther <belthergabriel@gmail.com>
2022-07-04 22:32:13 -0300

    feat: ensure ValidationComposite return error if any validator fails

---
+++ src/main/composites/validation-composite.ts
@@ -0,0 +1,20 @@
+import { Validation } from '@/presentation/contracts';
+import { FieldValidator } from '@/validation/contracts';
+
+export class ValidationComposite implements Validation {
+  constructor(private readonly validators: FieldValidator[]) {}
+
+  validate(fieldName: string, input: object): string {
+    const validators = this.validators.filter(
+      validator => validator.field === fieldName,
+    );
+
+    if (validators.length > 0) {
+      for (const validator of validators) {
+        const error = validator.validate(input);
+        if (error) return error.message;
+      }
+    }
+    return '';
+  }
+}
