5e7f13ec7f2b4910f5c6c3e4ada6e4cfa23f4c94
gbelther <belthergabriel@gmail.com>
2022-07-05 21:05:08 -0300

    test: ensure return EmailValidator and a list of validators

--- src/main/builders/validation-builder.spec.ts
+++ src/main/builders/validation-builder.spec.ts
@@ -1,11 +1,29 @@
 import { faker } from '@faker-js/faker';
 import { RequiredFieldValidator } from '@/validation/validators/required-field-validator';
 import { ValidationBuilder as sut } from './validation-builder';
+import { EmailValidator } from '@/validation/validators/email-validator';
 
 describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidator', () => {
     const field = faker.database.column();
     const validations = sut.field(field).required().build();
     expect(validations).toEqual([new RequiredFieldValidator(field)]);
+  });
+
+  test('Should return EmailValidator', () => {
+    const field = faker.database.column();
+    const validations = sut.field(field).email().build();
+    expect(validations).toEqual([new EmailValidator(field)]);
+  });
+
+  test('Should return a list of validators', () => {
+    const field = faker.database.column();
+
+    const validations = sut.field(field).required().email().build();
+
+    expect(validations).toEqual([
+      new RequiredFieldValidator(field),
+      new EmailValidator(field),
+    ]);
   });
 });
