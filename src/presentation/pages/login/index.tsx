38150b416656e54886e026d456bd3c882f1240d3
gbelther <belthergabriel@gmail.com>
2022-07-07 21:42:54 -0300

    feat: create state for inputs values

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,14 +1,40 @@
+import { ChangeEvent, useState } from 'react';
 import * as Sty from './styles';
 
 export function Login() {
+  const [loginFormValues, setLoginFormValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleLoginValuesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginFormValues(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <Sty.Container>
       <Sty.LoginBox>
         <Sty.Title>Faça login</Sty.Title>
         <Sty.Form>
           <Sty.Inputs>
-            <Sty.Input type="email" placeholder="Digite seu E-mail" />
-            <Sty.Input type="password" placeholder="Digite sua senha" />
+            <Sty.Input
+              type="email"
+              placeholder="Digite seu E-mail"
+              name="email"
+              value={loginFormValues.email}
+              onChange={handleLoginValuesChange}
+            />
+            <Sty.Input
+              type="password"
+              placeholder="Digite sua senha"
+              name="password"
+              value={loginFormValues.password}
+              onChange={handleLoginValuesChange}
+            />
           </Sty.Inputs>
           <Sty.Feedback>
             <Sty.SpinnerWrap>
