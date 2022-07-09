4a16dd5358bba24efd6988ce0748e713624b32ac
gbelther <belthergabriel@gmail.com>
2022-07-09 12:33:20 -0300

    feat: ensure call localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,11 +1,16 @@
-import { SetStorage } from '@/data/contracts/cache';
+import { GetStorage, SetStorage } from '@/data/contracts/cache';
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, GetStorage {
   set(key: string, value: object): void {
     if (value) {
       localStorage.setItem(key, JSON.stringify(value));
     } else {
       localStorage.removeItem(key);
     }
+  }
+
+  get(key: string): any {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : item;
   }
 }
