a83314017cdf9ac1ea102f4ad7439caab36e74fb
gbelther <belthergabriel@gmail.com>
2022-07-09 14:17:02 -0300

    feat: ensure call HttpClient with correct url

--- src/data/usecases/remote-load-task-list.ts
+++ src/data/usecases/remote-load-task-list.ts
@@ -2,11 +2,14 @@ import { LoadTaskList } from '@/domain/usecases';
 import { HttpClient } from '../contracts/http';
 
 export class RemoteLoadTaskList implements LoadTaskList {
-  constructor(private readonly httpClient: HttpClient<LoadTaskList.Model>) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<LoadTaskList.Model>,
+  ) {}
 
   async loadAll(): Promise<LoadTaskList.Model[]> {
     await this.httpClient.request({
-      url: '',
+      url: this.url,
       method: 'GET',
     });
     return null;
