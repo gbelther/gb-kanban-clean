03b67607490b4869ac15339b00ee13646e0f39db
gbelther <belthergabriel@gmail.com>
2022-07-09 12:29:38 -0300

    feat: ensure call localStorage.remoteItem if value is falsy

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -2,6 +2,10 @@ import { SetStorage } from '@/data/contracts/cache';
 
 export class LocalStorageAdapter implements SetStorage {
   set(key: string, value: object): void {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.removeItem(key);
+    }
   }
 }
