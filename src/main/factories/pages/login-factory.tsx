a9a37a59750c42449ff47e20d92100a4b90b65ff
gbelther <belthergabriel@gmail.com>
2022-07-10 23:04:56 -0300

    refactor: change context session account to global app

--- src/main/factories/pages/login-factory.tsx
+++ src/main/factories/pages/login-factory.tsx
@@ -1,13 +1,10 @@
 import { Login } from '@/presentation/pages/login';
-import { SessionAccountContextProvider } from '@/presentation/contexts/session-account-context';
 import { makeRemoteAuthentication } from '../usecases';
 import { makeLoginValidation } from '../validation';
 
 export const makeLogin = () => (
-  <SessionAccountContextProvider>
-    <Login
-      validation={makeLoginValidation()}
-      authentication={makeRemoteAuthentication()}
-    />
-  </SessionAccountContextProvider>
+  <Login
+    validation={makeLoginValidation()}
+    authentication={makeRemoteAuthentication()}
+  />
 );
--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -3,16 +3,18 @@ import {
   Routes as RoutesContainer,
   Route,
 } from 'react-router-dom';
-
+import { SessionAccountContextProvider } from '@/presentation/contexts/session-account-context';
 import { makeBoard, makeLogin } from '../factories/pages';
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <RoutesContainer>
-        <Route path="/login" element={makeLogin()} />
-        <Route path="/" element={makeBoard()} />
-      </RoutesContainer>
-    </BrowserRouter>
+    <SessionAccountContextProvider>
+      <BrowserRouter>
+        <RoutesContainer>
+          <Route path="/login" element={makeLogin()} />
+          <Route path="/" element={makeBoard()} />
+        </RoutesContainer>
+      </BrowserRouter>
+    </SessionAccountContextProvider>
   );
 }
