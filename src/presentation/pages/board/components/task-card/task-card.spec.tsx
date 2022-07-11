26e0c6fc00f9fb0290deb8057cf8bb13156aa789
gbelther <belthergabriel@gmail.com>
2022-07-10 22:36:33 -0300

    test: ensure render content correctly

--- src/presentation/pages/board/components/task-card/task-card.spec.tsx
+++ src/presentation/pages/board/components/task-card/task-card.spec.tsx
@@ -3,17 +3,28 @@ import { faker } from '@faker-js/faker';
 import { renderTheme } from '@/main/config/tests/renderTheme';
 import { TaskCard } from '.';
 
-const makeSut = (
+type MakeSutParams = {
+  title?: string;
+  content?: string;
+};
+
+const makeSut = ({
   title = faker.random.word(),
   content = faker.random.words(),
-): void => {
+}: MakeSutParams): void => {
   renderTheme(<TaskCard title={title} content={content} />);
 };
 
 describe('<TaskCard />', () => {
   it('should render title correctly', () => {
     const title = faker.random.word();
-    makeSut(title);
+    makeSut({ title });
     expect(screen.queryByText(title)).toBeTruthy();
+  });
+
+  it('should render content correctly', () => {
+    const content = faker.random.words();
+    makeSut({ content });
+    expect(screen.queryByText(content)).toBeTruthy();
   });
 });
