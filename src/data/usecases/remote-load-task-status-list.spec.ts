b683041f94df14242e7b5c3fd18b113daab8f8e1
gbelther <belthergabriel@gmail.com>
2022-07-11 18:47:44 -0300

    test: create remote load-task-status-list

---
+++ src/data/usecases/remote-load-task-status-list.spec.ts
@@ -0,0 +1,112 @@
+/* eslint-disable no-plusplus */
+import { faker } from '@faker-js/faker';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
+import {
+  HttpClient,
+  HttpRequest,
+  HttpResponse,
+  HttpStatusCode,
+} from '../contracts/http';
+import { RemoteLoadTaskStatusList } from './remote-load-task-status-list';
+import { LoadTaskStatusList } from '@/domain/usecases';
+
+class HttpClientSpy implements HttpClient {
+  url: string;
+  method: string;
+  body: any;
+
+  response: HttpResponse = {
+    statusCode: 200,
+    data: JSON.parse(faker.datatype.json()),
+  };
+
+  async request(data: HttpRequest): Promise<HttpResponse<any>> {
+    this.url = data.url;
+    this.method = data.method;
+    this.body = data.body;
+
+    return this.response;
+  }
+}
+
+const makeRemoteTaskStatusListModel = (
+  length = 2,
+): LoadTaskStatusList.Model[] => {
+  const list: LoadTaskStatusList.Model[] = [];
+  for (let i = 0; i < length; i++) {
+    list.push({
+      id: faker.datatype.uuid(),
+      name: faker.random.word(),
+      order: i + 1,
+    });
+  }
+  return list;
+};
+
+type SutTypes = {
+  sut: RemoteLoadTaskStatusList;
+  httpClientSpy: HttpClientSpy;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteLoadTaskStatusList(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
+describe('RemoteLoadTaskList', () => {
+  it('should call HttpClient with correct method', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    await sut.loadAll();
+    expect(httpClientSpy.method).toBe('GET');
+  });
+
+  it('should call HttpClient with correct url', async () => {
+    const url = faker.internet.url();
+    const { sut, httpClientSpy } = makeSut(url);
+    await sut.loadAll();
+    expect(httpClientSpy.url).toBe(url);
+  });
+
+  it('should throw AccessDeniedError if HttpClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+    const loadAllPromise = sut.loadAll();
+    await expect(loadAllPromise).rejects.toThrow(new AccessDeniedError());
+  });
+
+  it('should throw UnexpectedError if HttpClient returns another statusCode error', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const loadAllPromise = sut.loadAll();
+    await expect(loadAllPromise).rejects.toThrow(new UnexpectedError());
+  });
+
+  it('should return a list of TaskModels if HttpClient returns 200', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    const length = faker.datatype.number(10);
+    const statusList = makeRemoteTaskStatusListModel(length);
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.success,
+      data: statusList,
+    };
+    const httpResponse = await sut.loadAll();
+    expect(httpResponse).toHaveLength(length);
+  });
+
+  it('should return an empty list if HttpClient returns 204', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.noContent,
+    };
+    const httpResponse = await sut.loadAll();
+    expect(httpResponse).toEqual([]);
+  });
+});
