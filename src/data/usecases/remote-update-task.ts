52a274e4db0f98c2f14a9923f5439818b536ecb5
gbelther <belthergabriel@gmail.com>
2022-07-16 14:07:58 -0300

    feat: ensure throw AccessDeniedError if HttpClient returns 403

--- src/data/usecases/remote-update-task.ts
+++ src/data/usecases/remote-update-task.ts
@@ -1,5 +1,6 @@
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
 import { UpdateTask } from '@/domain/usecases';
-import { HttpClient } from '../contracts/http';
+import { HttpClient, HttpStatusCode } from '../contracts/http';
 
 export class RemoteUpdateTask implements UpdateTask {
   constructor(
@@ -8,11 +9,19 @@ export class RemoteUpdateTask implements UpdateTask {
   ) {}
 
   async update(data: UpdateTask.Params): Promise<UpdateTask.Model> {
-    await this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       url: `${this.url}/${data.id}`,
       method: 'PATCH',
       body: data,
     });
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.success:
+        return httpResponse.data;
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
     return null;
   }
 }
